89565d5b3a2985278e395a857758728559832eb8
Mohammad Altiwainy <153241295+maltiwainy229@users.noreply.github.com>
2025-08-13 17:02:34 -0500

    Create appStruct.swift

---
+++ appStruct.swift
@@ -0,0 +1,17 @@
+//
+//  ChatBotgeminiApp.swift
+//  ChatBotgemini
+//
+//  Created by Ali Hamdani on 8/2/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatBotgeminiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
